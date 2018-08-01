1511c6fb69bd3e8362412a358633ddd894a45732
Cihat Gündüz <cihat.guenduez@jamitlabs.com>
2018-08-01 13:52:42 +0200

    Add shared variables support to configuration file

--- Sources/ProjLintKit/Globals/RuleFactory.swift
+++ Sources/ProjLintKit/Globals/RuleFactory.swift
@@ -1,8 +1,8 @@
 import Foundation
 
 enum RuleFactory {
-    static func makeRule(identifier: String, optionsDict: [String: Any]?) -> Rule {
-        let optionsDict = optionsDict ?? [:]
+    static func makeRule(identifier: String, optionsDict: [String: Any]?, sharedVariables: [String: String]?) -> Rule {
+        let optionsDict = sharedVariablesAppliedDict(optionsDict ?? [:], sharedVariables: sharedVariables ?? [:])
 
         switch identifier {
         case FileExistenceRule.identifier:
@@ -15,5 +15,38 @@ enum RuleFactory {
             print("Rule with identifier \(identifier) unknown.", level: .error)
             exit(EX_USAGE)
         }
+    }
+
+    private static func sharedVariablesAppliedDict(_ dictionary: [String: Any], sharedVariables: [String: String]) -> [String: Any] {
+        var newDict = [String: Any]()
+
+        for (key, value) in dictionary {
+            let newKey = sharedVariablesAppliedString(key, sharedVariables: sharedVariables)
+            let newValue: Any = {
+                if let stringValue = value as? String {
+                    return sharedVariablesAppliedString(stringValue, sharedVariables: sharedVariables)
+                } else if let stringArrayValue = value as? [String] {
+                    return stringArrayValue.map { sharedVariablesAppliedString($0, sharedVariables: sharedVariables) }
+                } else if let dictValue = value as? [String: Any] {
+                    return sharedVariablesAppliedDict(dictValue, sharedVariables: sharedVariables)
+                } else {
+                    return value
+                }
+            }()
+
+            newDict[newKey] = newValue
+        }
+
+        return newDict
+    }
+
+    private static func sharedVariablesAppliedString(_ string: String, sharedVariables: [String: String]) -> String {
+        var newString = string
+
+        for (placeholder, replacement) in sharedVariables {
+            newString = newString.replacingOccurrences(of: "<:\(placeholder):>", with: replacement)
+        }
+
+        return newString
     }
 }
