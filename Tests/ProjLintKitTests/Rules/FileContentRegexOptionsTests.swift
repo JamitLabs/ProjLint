4b5eae6ec5ce0e7571fd47eeb7f773ede4677c24
Cihat Gündüz <cihat.guenduez@jamitlabs.com>
2018-08-02 13:27:50 +0200

    Implement first options test using Faker

--- Tests/ProjLintKitTests/Rules/FileContentRegexOptionsTests.swift
+++ Tests/ProjLintKitTests/Rules/FileContentRegexOptionsTests.swift
@@ -1,9 +1,16 @@
+import HandySwift
 @testable import ProjLintKit
 import XCTest
 
 final class FileContentRegexOptionsTests: XCTestCase {
     func testInitWithMatchingRegex() {
-        // TODO: not yet implemented
+        let valueType = FakerType.dict(keyType: .filePath, valueType: .regexString, count: 5)
+        let optionsDict = ["matching": Faker.first.data(ofType: valueType)]
+
+        let options = FileContentRegexOptions(optionsDict, rule: FileContentRegexRule.self)
+
+        XCTAssert(options.matchingRegex != nil)
+        XCTAssertEqual(options.matchingRegex!.count, 5)
     }
 
     func testInitWithMatchingAllPathRegexes() {
