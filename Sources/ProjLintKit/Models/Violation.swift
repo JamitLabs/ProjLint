5f01a4f5f964f911dc540460bf8ac8287d0ede26
Cihat Gündüz <cihat.guenduez@jamitlabs.com>
2018-08-10 13:41:30 +0200

    Add support for printing in Xcode compatible manner

--- Sources/ProjLintKit/Models/Violation.swift
+++ Sources/ProjLintKit/Models/Violation.swift
@@ -13,6 +13,6 @@ class Violation {
     }
 
     func logViolation() {
-        print("\(type(of: rule).name) Violation – \(message)", level: level.printLevel) // swiftlint:disable:this leveled_print
+        print("\(type(of: rule).name) Violation – \(message)", level: level.printLevel, file: nil, line: nil)
     }
 }
