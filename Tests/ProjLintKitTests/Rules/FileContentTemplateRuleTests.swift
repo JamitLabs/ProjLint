5ea283d816c7ea8b4eabb1246ea935b2647daae6
Cihat Gündüz <cihat.guenduez@jamitlabs.com>
2018-09-10 13:34:41 +0200

    Remove remote dependent test

--- Tests/ProjLintKitTests/Rules/FileContentTemplateRuleTests.swift
+++ Tests/ProjLintKitTests/Rules/FileContentTemplateRuleTests.swift
@@ -89,43 +89,4 @@ final class FileContentTemplateRuleTests: XCTestCase {
             XCTAssert(violations.first is FileViolation)
         }
     }
-
-    func testMatchingPathTemplateViaURL() {
-        resourcesLoaded([swiftlintConfigExample]) {
-            let optionsDict = [
-                "matching": [
-                    swiftlintConfigExample.path: [
-                        "template_url": "https://raw.githubusercontent.com/JamitLabs/ProjLint/stable/Resources/SwiftLint.stencil?token=AGnt0DJsNntcNAUusu2XcEkyoEb5T9Plks5bcqqMwA%3D%3D", // swiftlint:disable:this line_length
-                        "parameters": [
-                            "additionalRules": ["attributes", "empty_count", "sorted_imports"],
-                            "lineLength": "160"
-                        ]
-                    ]
-                ]
-            ]
-            let rule = FileContentTemplateRule(optionsDict)
-
-            let violations = rule.violations(in: Resource.baseUrl)
-            XCTAssertEqual(violations.count, 0)
-        }
-
-        resourcesLoaded([swiftlintConfigExample, swiftlintConfigTemplate]) {
-            let optionsDict = [
-                "matching": [
-                    swiftlintConfigExample.path: [
-                        "template_url": "https://raw.githubusercontent.com/JamitLabs/ProjLint/stable/Resources/SwiftLint.stencil?token=AGnt0DJsNntcNAUusu2XcEkyoEb5T9Plks5bcqqMwA%3D%3D", // swiftlint:disable:this line_length
-                        "parameters": [
-                            "additionalRules": ["attributes", "sorted_imports", "yoda_condition"],
-                            "lineLength": "80"
-                        ]
-                    ]
-                ]
-            ]
-            let rule = FileContentTemplateRule(optionsDict)
-
-            let violations = rule.violations(in: Resource.baseUrl)
-            XCTAssertEqual(violations.count, 1)
-            XCTAssert(violations.first is FileViolation)
-        }
-    }
 }
