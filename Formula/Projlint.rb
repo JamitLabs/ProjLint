7ddc676952018ca339e84a9cb816bcf7b4d0cbfc
Cihat Gündüz <cihat.guenduez@jamitlabs.com>
2019-02-22 14:57:35 +0100

    [Formula] Fix sha hash

--- Formula/Projlint.rb
+++ Formula/Projlint.rb
@@ -1,7 +1,7 @@
 class Projlint < Formula
   desc "Project Linter to enforce your non-code best practices"
   homepage "https://github.com/JamitLabs/ProjLint"
-  url "https://github.com/JamitLabs/ProjLint.git", :tag => "0.1.5", :revision => "94b675d65e2d1f1c555f07f5ccbe683677f2cf38"
+  url "https://github.com/JamitLabs/ProjLint.git", :tag => "0.1.5", :revision => "fc237787482c6d6e8d895954ef8e40cdc058ed99"
   head "https://github.com/JamitLabs/ProjLint.git"
 
   depends_on :xcode => ["10.0", :build]
