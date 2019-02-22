bb1bfa92c67302e526faa08472eecaf8ce25ea74
Cihat Gündüz <cihat.guenduez@jamitlabs.com>
2019-02-22 14:56:52 +0100

    Update formula to version 0.1.5

--- Formula/Projlint.rb
+++ Formula/Projlint.rb
@@ -1,7 +1,7 @@
 class Projlint < Formula
   desc "Project Linter to enforce your non-code best practices"
   homepage "https://github.com/JamitLabs/ProjLint"
-  url "https://github.com/JamitLabs/ProjLint.git", :tag => "0.1.4", :revision => "7e35b1fbb9ec37d0b01a1190eb5c00ebdac4975b"
+  url "https://github.com/JamitLabs/ProjLint.git", :tag => "0.1.5", :revision => "94b675d65e2d1f1c555f07f5ccbe683677f2cf38"
   head "https://github.com/JamitLabs/ProjLint.git"
 
   depends_on :xcode => ["10.0", :build]
